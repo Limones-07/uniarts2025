aec9291feeb8431436b0573f66ca8c4220f22817
Limones <07limones@gmail.com>
2025-05-05 21:09:21 -0300

    PROOF OF CONCEPT: Server and ServerInterface resilient to scene change (see server_testing_scene.gd).

--- scenes/server_testing_scene/server_testing_scene.gd
+++ scenes/server_testing_scene/server_testing_scene.gd
@@ -9,6 +9,8 @@ func _ready() -> void:
 	_log("Starting testing sequence. Good luck!\n")
 	await _test_defaults()
 	_log("\n")
+	await _test_defaults_2()
+	_log("\n")
 	await _test_localhost()
 	_log("\n")
 	await _test_different_port()
@@ -26,6 +28,22 @@ func _test_defaults() -> void:
 	_log("Initializing the interface with default values (127.0.0.1:35565)...")
 	_server_interface.init.emit()
 	await _wait()
+	_log("Destroying the server...")
+	_server_interface.destroy_server.emit()
+	await _server_interface.server_destroyed
+	#await _give_some_time_to_the_os_so_socket_binding_on_the_same_port_of_last_time_doesnt_fail()
+
+func _test_defaults_2() -> void:
+	_log("Spawning server with default value (port 35565)...")
+	_server_interface.spawn_server.emit()
+	await _wait()
+	_log("Initializing the interface with default values (127.0.0.1:35565)...")
+	_server_interface.init.emit()
+	await _wait()
+	_log("Reloading the main scene...")
+	self.get_tree().change_scene_to_file("res://scenes/main.tscn")
+	_log("Waiting 10 seconds...")
+	await _wait(10)
 	_log("Destroying the server...")
 	_server_interface.destroy_server.emit()
 	await _server_interface.server_destroyed
